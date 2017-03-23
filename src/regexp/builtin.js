ada8bebd39cdf47be6f05d7f9d0274bc3e941d1d
mo <feng_zilong@163.com>
2017-03-23 09:36:54 +0800

    Update regexp

--- src/regexp/builtin.js
+++ src/regexp/builtin.js
@@ -1,3 +1,5 @@
 // https://www.w3.org/TR/1999/REC-xml-names-19990114/#NT-QName
 const ncname = '[a-zA-Z_][\\w\\-\\.]*';
-export const TAG_NAME = `(?:${ ncname }\\:)?${ ncname }`;
+export default {
+	TAG_NAME: `(?:${ ncname }\\:)?${ ncname }`,
+};
--- src/regexp/compile.js
+++ src/regexp/compile.js
@@ -1,3 +1,13 @@
-export default function () {
-	
+import builtin from './builtin';
+
+export default function ( regexp = new RegExp(), map = {} ) {
+	map = Object.assign( {}, builtin, map );
+	const regStr = regexp.source.replace(
+		/{{([_-\w]+)}}/g,
+		function ( all, key ) {
+			return map[ key ];
+		}
+	);
+
+	return new RegExp( regStr, regexp.flags );
 }
