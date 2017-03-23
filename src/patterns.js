10556ca05bff319f486816f43daf17d4bbbddbd7
mo <feng_zilong@163.com>
2017-03-23 23:45:53 +0800

    Update patterns

--- src/patterns.js
+++ src/patterns.js
@@ -2,9 +2,10 @@ import compile from './regexp/compile';
 
 export default {
 	TAG_OPEN: compile( /^<({{TAG_NAME}})\s*/ ),
+	ATTRIBUTE: /^([-@:\.0-9a-z\(\)\[\]]+)(=(['"])*([^\3]*?)\3)?\s*/,
+	TAG_OPEN_END: /^\/?>/,
 	TAG_CLOSE: compile( /^<\/({{TAG_NAME}})>/ ),
-	'>': /^>/,
-	ATTRIBUTE: /^([-:0-9a-z\(\)\[\]]+)(=(['"])*([^\3]*?)\3)?\s*/,
+	TAG_COMMENT: /^<\!--([^\x00]*?)--\>/,
 	TEXT: /^[^\x00]/,
 	SPACE: /^[\r\n\t ]+/,
 };
