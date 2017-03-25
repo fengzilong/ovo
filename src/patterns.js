67dea65f0ef9b094a3f0d0089dde4b24ebf29483
mo <feng_zilong@163.com>
2017-03-25 20:57:14 +0800

    Add mustache patterns

--- src/patterns.js
+++ src/patterns.js
@@ -1,11 +1,29 @@
 import compile from './regexp/compile';
 
+/* eslint-disable */
 export default {
+	// TAG
 	TAG_OPEN: compile( /^<({{TAG_NAME}})\s*/ ),
 	ATTRIBUTE: /^([-@:\.0-9a-z\(\)\[\]]+)(=(['"])*([^\3]*?)\3)?\s*/,
-	TAG_OPEN_END: /^\/?>/,
+	TAG_END: /^(\/?)>/,
 	TAG_CLOSE: compile( /^<\/({{TAG_NAME}})>/ ),
 	TAG_COMMENT: /^<\!--([^\x00]*?)--\>/,
+
+	// Mustache
+	MUSTACHE_OPEN: compile( /^{{BEGIN}}#({{IDENT}})\s*/ ),
+	MUSTACHE_END: compile( /^{{END}}/ ),
+	MUSTACHE_CLOSE: compile( /^{{BEGIN}}\/{{IDENT}}{{END}}/ ),
+	MUSTACHE_EXPRESSION_OPEN: compile( /^{{BEGIN}}/ ),
+	MUSTACHE_EXPRESSION_IDENT: compile( /^({{IDENT}})/ ),
+	MUSTACHE_EXPRESSION_NUMBER: /^((?:\d*\.\d+|\d+)(?:e\d+)?)/,
+	MUSTACHE_EXPRESSION_STRING: /^(['"])([^\1]*?)\1/,
+	MUSTACHE_EXPRESSION_SYMBOL: /^([=!]?==|[-=><+*\/%\!]?\=|\|\||&&|[<\>\[\]\(\)\-\|\+\*\/%?:\.!,])/,
+	MUSTACHE_EXPRESSION_BRACE_OPEN: /^([{])/,
+	MUSTACHE_EXPRESSION_BRACE_END: /^([}])/,
+
+	// Others
 	TEXT: /^[^\x00]/,
-	SPACE: /^[\r\n\t ]+/,
+	SPACE: /^\s+/,
+	EOS: /^$/,
 };
+/* eslint-enable */
--- src/regexp/builtin.js
+++ src/regexp/builtin.js
@@ -1,5 +1,9 @@
 // https://www.w3.org/TR/1999/REC-xml-names-19990114/#NT-QName
 const ncname = '[a-zA-Z_][\\w\\-\\.]*';
+
 export default {
 	TAG_NAME: `(?:${ ncname }\\:)?${ ncname }`,
+	IDENT: `[\\$_a-zA-Z][\\$_0-9a-zA-Z]*`,
+	BEGIN: `{`,
+	END: `}`,
 };
