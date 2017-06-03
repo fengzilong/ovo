dc8a5656a903c2905d6b129eae550484276ddd24
MO <fengzilong1992@gmail.com>
2017-06-03 19:13:48 +0800

    Cleanup

--- src/template/nodes/slot.js
+++ src/template/nodes/slot.js
@@ -1,5 +1,3 @@
-export default class Slot {
-	constructor() {
-		
-	}
+export default function () {
+	return {};
 }
--- src/template/nodes/tag.js
+++ src/template/nodes/tag.js
@@ -1,5 +1,3 @@
-import Node from '../../shared/node';
-
 export default function ( { name = '', attributes = {}, children = [], isSelfClosed = false } ) {
 	return {
 		name,
